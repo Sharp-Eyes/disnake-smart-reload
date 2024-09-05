b2dcb34bc83b782cc6ccf7b105eccf9abe69ab7c
Chromosomologist <chromosomologist@gmail.com>
2024-09-05 09:58:30 +0200

    fix: modify parser name resolution

--- src/smart_reload/parser.py
+++ src/smart_reload/parser.py
@@ -8,6 +8,37 @@ import sys
 import types
 
 
+def resolve_name(
+    module: str | None,
+    name: str,
+    package: str | None,
+    level: int,
+) -> str:
+    """Resolve parts of an import statement to an absolute import."""
+    fullname = f"{module}.{name}" if module else name
+
+    if not package:
+        if not fullname.startswith("."):
+            # TODO: remove after testing or maybe log instead?
+            print(f"resolved {{{module=}, {name=}, {package=}, {level=}}} to {fullname!r}")
+            return fullname
+
+        # Should essentially never happen as the modules passed actual importing;
+        # no package specified for relative import
+        raise RuntimeError
+
+    base, *remainder = package.rsplit(".", level - 1)
+    if len(remainder) == level:
+        # Should essentially never happen as the modules passed actual importing;
+        # attempted relative import beyond top-level package
+        raise RuntimeError
+
+    resolved = f"{base}.{fullname}" if name else base
+    # TODO: remove after testing or maybe log instead?
+    print(f"resolved {{{module=}, {name=}, {package=}, {level=}}} to {resolved!r}.")
+    return resolved
+
+
 def parse_module(path: str) -> ast.Module:
     """Parse a module given its path, returning an ast object
     representing its source.
@@ -24,48 +55,22 @@ class ModuleVisitor(ast.NodeVisitor):
         self.package: str | None = package
         self.imported_modules: set[str] = set()
 
-    def _resolve_relative_package(self, name: str, level: int) -> str:
-        return importlib._bootstrap._resolve_name(  # type: ignore
-            name,
-            package=self.package,
-            level=level,
-        )
-
     def visit_Import(self, node: ast.Import) -> None:
-        for import_name in node.names:
-            self.imported_modules.add(import_name.name)
+        for alias in node.names:
+            resolved = resolve_name(None, alias.name, self.package, 0)
+            if resolved in sys.modules:
+                self.imported_modules.add(resolved)
 
     def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
-        if node.level:
-            # relative import
-            # e.g from .Y import X, etc...
-            for alias in node.names:
-                if node.module:
-                    name = node.module + "." + alias.name
-                else:
-                    name = alias.name
+        if node.module:
+            resolved = resolve_name(None, node.module, self.package, node.level)
+            if resolved in sys.modules:
+                self.imported_modules.add(resolved)
 
-                if self.package:
-                    self.imported_modules.add(
-                        self._resolve_relative_package(name, node.level)
-                    )
-                else:
-                    self.imported_modules.add(name)
-        else:
-            # non-relative import
-            # e.g from Y import X, etc...
-            if not node.module:
-                # should never happen
-                return
-
-            for alias in node.names:
-                if not alias.asname:
-                    continue
-                if isinstance(
-                    sys.modules[node.module].__dict__[alias.asname], types.ModuleType
-                ):
-                    self.imported_modules.add(node.module + "." + alias.asname)
-            self.imported_modules.add(node.module)
+        for alias in node.names:
+            resolved = resolve_name(node.module, alias.name, self.package, node.level)
+            if resolved in sys.modules:
+                self.imported_modules.add(resolved)
 
 
 class Parser:
